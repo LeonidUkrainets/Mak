73764f42ec9b2482eaef02fd1e26d21df6d0e04e
LeonidUkrainets <bookovina@gmail.com>
2016-09-07 16:59:38 +0300

    Update rekvest.js

--- rekvest.js
+++ rekvest.js
@@ -1,7 +1,7 @@
 
 var xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://github.com/proxy/macvendors.co/api/vendorname/D4:CA:6D:2A:C3:8F', false);
+xhr.open('GET', 'https://github.com/proxy/macvendors.co/api/vendorname/D4:CA:6D:2A:C3:8F',true);
 
 xhr.send();
 
