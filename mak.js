e3e93cd710586a281fc24a6e428ee55b7807b6f1
LeonidUkrainets <bookovina@gmail.com>
2017-02-10 17:20:33 +0200

    1.2.2

--- mak.js
+++ mak.js
@@ -1,50 +1,22 @@
-//Версія 1.5.7 від 2017/02/09
-var vyrobn= ['Edimax Technology Co. Ltd.',
-             'Tenda Technology Co., Ltd.',
-             'TP-LINK TECHNOLOGIES CO.,LTD.',
-             'NETCORE SYSTEMS, INC.',
-             'Netcore Technology Inc.', 
-             'ASUSTek COMPUTER INC.'];
+//Версія 1.2.2 від 2016/06/30
 var abetka= ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','A','B','C','D','E','F'];
 var adresa=''; // початкова адреса, отримана шляхом введення
 var adreso=''; // кінцева адреса
-var apireq='https://macvendors.co/api/vendorname/';
-
-function getRndmInteger(min, max) {
-    return Math.floor(Math.random() * (max - min+1)) + min;
-}
-// перевіряє, чи введені символи відповідають тим, що можуть бути в мак-адресі.
-function perevirkaAdres(makadresa){
-  pravylna=true;
-  for (i=0;i<makadresa.length;i++)
-    {if (abetka.indexOf(makadresa[i])<0) pravylna=false;}
-  return pravylna;
-}
-
-var zap=function() 
- {
-      $.ajax({
-        type: 'GET',
-        url: 'https://macvendors.co/api/jsonp/'+adreso,
-        dataType: 'jsonp',
-        crossDomain: true,
-    }).done(function(response){
-		if(response.result.error){
-            $('.list').append('<div class="item">'+response.result.error+ '</div>');
-			
-		}else{
-			$('.list').append('<div class="item"><a href=http://192.168.'+ip+'.1 target="_blank"><p class="name">' +response.result.company+'</p></a></div>');
-		
-		}
-    }).fail(function(error){
-    
-    });
- };
-
 var pro=function()
-  if (perevirkaAdres(adresa)===true) 
-  {
-      //$('.list').append('<div class="item">' + adresa + '</div>'); вивід адреси без розділових знаків
+ {
+  for (i=0;i<12;i++)
+  {  
+     var find_error=true;
+     var j=0;
+     while (j<abetka.length && find_error)
+        {
+            if ( adresa[i]===abetka[j] )
+              {find_error=false;}
+            j++;
+        }
+  }
+  if (find_error===false) 
+  {   //$('.list').append('<div class="item">' + adresa + '</div>'); вивід адреси без розділових знаків
       adreso=adresa.substring(0,4);
       i=4;
       do
@@ -65,17 +37,23 @@ var pro=function()
       {adreso=adreso+':'+adresa.substring(i,i+2);
       i+=2;}
       while (i<adresa.length)
-   
       $('.list').append('<div class="item">' + adreso + '</div>');
-      $('.list').append('<div class="item">sudo -s</div>');
-      $('.list').append('<div class="item">vconfig add '+iface+' '+vlan+'</div>');
-      $('.list').append('<div class="item">ifconfig    '+iface+'.'+vlan+' inet 192.168.'+ip+'.'+ip_comp+'</div>');
-      $('.list').append('<div class="item">arp -s 192.168.'+ip+'.1 '+ adreso+'</div>');
-      $('.list').append('<div class="item">arping -I        '+iface+'.'+vlan+' -b 192.168.'+ip+'.1</div>');
-      $('.list').append('<div class="item">sudo vconfig rem '+iface+'.'+vlan+'</div>');
-      zap();
-    //  $('.list').append('<div class="item"><a href=https://macvendors.co/api/vendorname/'+adreso+' target="_blank"><p class="name" >Дізнатись виробника</p></a><br></div>');
-    //  $('.list').append('<div class="item"><a href=http://192.168.'+ip+'.1 target="_blank"><p class="name"> Перейти до налаштувань роутера</p></a></div>');
+      $('.list').append('<div class="item">sudo vconfig add eth1 '+vlan+'</div>');
+      $('.list').append('<div class="item">sudo ifconfig    eth1.'+vlan+' inet 192.168.0.33</div>');
+      $('.list').append('<div class="item">sudo arp -s 192.168.0.1 '+ adreso+'</div>');
+      $('.list').append('<div class="item">arping -I        eth1.'+vlan+' -b 192.168.0.1</div>');
+      $('.list').append('<div class="item">sudo vconfig rem eth1.'+vlan+'</div>');
+      $('.list').append('<div class="item"><h4>Деякі корисні команди для олта BDCOM<h4></div>');
+      $('.list').append('<div class="item">show epon interface EPON0/1:2 onu ctc optical-transceiver-diagnosis</div>');
+      $('.list').append('<div class="item">show epon optical-transceiver-diagnosis</div>');
+      $('.list').append('<div class="item">show mac address-table interface EPON 0/1:2</div>');
+      $('.list').append('<div class="item">show mac address-table dynamic vlan '+vlan+'</div>');
+      $('.list').append('<div class="item">show vlan id '+vlan+'</div>');
+      $('.list').append('<div class="item">show interface brief</div>');
+      $('.list').append('<div class="item">show epon onu-information</div>');
+      $('.list').append('<div class="item">show epon active-onu</div>');
+      $('.list').append('<div class="item">show epon inactive-onu</div>');
+
   }
   else $('.list').append('<div class="item">' + 'Хибна адреса. Спробуй ще раз' + '</div>');
  };
@@ -85,16 +63,10 @@ $(document).ready(function ()
     $('#button').click(function ()
         {
         $(".item").remove();
-        adreso='';
-        ip    =$('input[name=ipadr]').val();
-        if (ip==='') 
-           {ip=0;}
-        vlan  =$('input[name=vlan]').val();
+        adreso=''
+        vlan=$('input[name=vlan]').val();
         adresa=$('input[name=checkListItem]').val();
-        iface =$('input[name=iface]').val();
-        if (iface==='') 
-           {iface='eth1';}
-        ip_comp=getRndmInteger(10, 99);
+  
         for (i=0;i<adresa.length;i++)
           {if (adresa[i]!==' ' && adresa.charCodeAt(i)!==9) {adreso=adreso+adresa[i];}}
         adresa=adreso;
