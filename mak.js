b9ae54200fdea20af073716bba8f8e976e5883ad
LeonidUkrainets <bookovina@gmail.com>
2017-02-13 15:45:07 +0200

    1.5.9

--- mak.js
+++ mak.js
@@ -1,29 +1,42 @@
-//Версія 1.2.2 від 2016/06/30
+//Версія 1.5.9 від 2017/02/13
+var vyrob1= ['Netcore Technology Inc.','ASUSTek COMPUTER INC.'];
 var abetka= ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','A','B','C','D','E','F'];
 var adresa=''; // початкова адреса, отримана шляхом введення
 var adreso=''; // кінцева адреса
-var pro=function()
+function getRndmInteger(min, max) {
+    return Math.floor(Math.random() * (max - min+1)) + min;
+};
+function perevirkaAdres(makadresa){
+  pravylna=true;
+  for (i=0;i<makadresa.length;i++)
+    {if (abetka.indexOf(makadresa[i])<0) pravylna=false;}
+  return pravylna;
+};
+var zap=function() 
  {
-  for (i=0;i<12;i++)
-  {  
-     var find_error=true;
-     var j=0;
-     while (j<abetka.length && find_error)
-        {
-            if ( adresa[i]===abetka[j] )
-              {find_error=false;}
-            j++;
-        }
-  }
-  if (find_error===false) 
-  {   //$('.list').append('<div class="item">' + adresa + '</div>'); вивід адреси без розділових знаків
-      adreso=adresa.substring(0,4);
-      i=4;
-      do
-      {adreso=adreso+'.'+adresa.substring(i,i+4);
-      i+=4;}
-      while (i<adresa.length)
-      $('.list').append('<div class="item"> show mac add ' + adreso + '</div>'); // команда для перевірки лінку з оптичним терміналом абонента
+      $.ajax({
+        type: 'GET',
+        url: 'https://macvendors.co/api/jsonp/'+adresa,
+        dataType: 'jsonp',
+        crossDomain: true,
+    }).done(function(response){
+		if(response.result.error){
+            $('.list').append('<div class="item">'+response.result.error+ '</div>');
+	
+		}else{
+            //if ((response.result.company) in vyrobn){
+            //   ip=1;}
+			$('.list').append('<div class="item"><a href=http://192.168.'+ip+'.1 target="_blank"><p class="name">' +response.result.company+'</p></a></div>');
+	
+		}
+    }).fail(function(error){
+    
+    });
+ };
+var pro=function()
+ { 
+  if (perevirkaAdres(adresa)===true) 
+  {   zap(); 
       adreso=adresa.substring(0,2);
       i=2;
       do
@@ -38,15 +51,22 @@ var pro=function()
       i+=2;}
       while (i<adresa.length)
       $('.list').append('<div class="item">' + adreso + '</div>');
-      $('.list').append('<div class="item">sudo vconfig add eth1 '+vlan+'</div>');
-      $('.list').append('<div class="item">sudo ifconfig    eth1.'+vlan+' inet 192.168.0.33</div>');
-      $('.list').append('<div class="item">sudo arp -s 192.168.0.1 '+ adreso+'</div>');
-      $('.list').append('<div class="item">arping -I        eth1.'+vlan+' -b 192.168.0.1</div>');
-      $('.list').append('<div class="item">sudo vconfig rem eth1.'+vlan+'</div>');
+      $('.list').append('<div class="item">sudo -s</div>');
+      $('.list').append('<div class="item">vconfig add '+iface+' '+vlan+'</div>');
+      $('.list').append('<div class="item">ifconfig    '+iface+'.'+vlan+' inet 192.168.'+ip+'.'+ip_comp+'</div>');
+      $('.list').append('<div class="item">arp -s 192.168.'+ip+'.1 '+ adreso+'</div>');
+      $('.list').append('<div class="item">arping -I        '+iface+'.'+vlan+' -b 192.168.'+ip+'.1</div>');
+      $('.list').append('<div class="item">vconfig rem '+iface+'.'+vlan+'</div>');
+      adreso=adresa.substring(0,4);
+      i=4;
+      do
+      {adreso=adreso+'.'+adresa.substring(i,i+4);
+      i+=4;}
+      while (i<adresa.length)
       $('.list').append('<div class="item"><h4>Деякі корисні команди для олта BDCOM<h4></div>');
-      $('.list').append('<div class="item">show epon interface EPON0/1:2 onu ctc optical-transceiver-diagnosis</div>');
-      $('.list').append('<div class="item">show epon optical-transceiver-diagnosis</div>');
-      $('.list').append('<div class="item">show mac address-table interface EPON 0/1:2</div>');
+      $('.list').append('<div class="item"> show mac add ' + adreso + '</div>'); // команда для перевірки лінку з оптичним терміналом абонента
+      $('.list').append('<div class="item">show mac add int epon 0/1:2</div>');
+      $('.list').append('<div class="item">show epon int epon '+'0/1:2'+' onu ctc opt</div>');
       $('.list').append('<div class="item">show mac address-table dynamic vlan '+vlan+'</div>');
       $('.list').append('<div class="item">show vlan id '+vlan+'</div>');
       $('.list').append('<div class="item">show interface brief</div>');
@@ -64,8 +84,14 @@ $(document).ready(function ()
         {
         $(".item").remove();
         adreso=''
-        vlan=$('input[name=vlan]').val();
+        iface =$('input[name=iface]').val();
+        if (iface==='') 
+           {iface='eth1';}
+        ip_comp=getRndmInteger(10, 99);
+        vlan  =$('input[name=vlan]').val();
         adresa=$('input[name=checkListItem]').val();
+        ip='0';
+        epom=$('input[name=epom]').val();
   
         for (i=0;i<adresa.length;i++)
           {if (adresa[i]!==' ' && adresa.charCodeAt(i)!==9) {adreso=adreso+adresa[i];}}
